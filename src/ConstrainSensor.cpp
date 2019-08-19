ee401d71cf843dbde8a1286cdb991054eeef1736
Mr. Alex <yuriy.alex.salimov@gmail.com>
2019-08-19 14:22:52 +0300

    Code refactoring.
    - optimized read() method;
    - updated documentation.

--- src/ConstrainSensor.cpp
+++ src/ConstrainSensor.cpp
@@ -1,3 +1,7 @@
+/**
+  Created by Yurii Salimov, May, 2018.
+  Released into the public domain.
+*/
 #include "ConstrainSensor.h"
 
 ConstrainSensor::ConstrainSensor(
@@ -14,6 +18,11 @@ ConstrainSensor::~ConstrainSensor() {
   delete this->origin;
 }
 
+/*
+See about the constrain(*) function:
+https://www.arduino.cc/reference/en/language/functions/math/constrain/
+*/
 int ConstrainSensor::read() {
-  return constrain(this->origin->read(), this->low, this->high);
+  const int originValue = this->origin->read();
+  return constrain(originValue, this->low, this->high);
 }
--- src/ConstrainSensor.h
+++ src/ConstrainSensor.h
@@ -1,18 +1,28 @@
 /**
-	ConstrainSensor.h - Reads a signal from a origin sensor,
-	constrains the signal and return it.
+  ConstrainSensor - class-wrapper allows to constraint
+  a signal value of origin Sensor instance.
 
-	Instantiation:
-		Sensor origin();
-		ConstrainSensor sensor(origin, 0, 100);
+  Instantiation:
+  Sensor* constrainSensor = new ConstrainSensor(
+    SENSOR, LOW, HIGH
+  );
 
-	Read signal:
-		int value = sensor.read();
+  Where,
+  SENSOR - origin Sensor instance.
+  LOW - the lower end of the constraint range.
+  HIGH - the upper end of the constraint range.
 
-	https://github.com/YuriiSalimov/AD_Sensors
+  Read signal:
+  int value = constrainSensor->read();
 
-	Created by Yurii Salimov, May, 2018.
-	Released into the public domain.
+  v.1.3.3
+  - optimized read() method;
+  - updated documentation.
+
+  https://github.com/YuriiSalimov/AD_Sensors
+
+  Created by Yurii Salimov, May, 2018.
+  Released into the public domain.
 */
 #ifndef CONSTRAIN_SENSOR_H
 #define CONSTRAIN_SENSOR_H
@@ -21,35 +31,41 @@
 
 class ConstrainSensor final : public Sensor {
 
-	private:
-		Sensor* origin;
-		int low;
-		int high;
+  private:
+    Sensor* origin;
+    int low;
+    int high;
 
-	public:
-		/**
-			Constructor.
-			@param origin - the origin sensor;
-			@param low - the lower end of the constraint range;
-			@param high - the upper end of the constraint range.
-		*/
-		ConstrainSensor(Sensor* origin, int low, int high);
+  public:
+    /**
+      Constructor
 
-		~ConstrainSensor();
+      @param origin - the origin sensor (not NULL)
+      @param low - the lower end of the constraint range
+      @param high - the upper end of the constraint range
+    */
+    ConstrainSensor(Sensor* origin, int low, int high);
 
-		/**
-			Reads a signal from the origin sensor,
-			constrains the signal and return it.
-			Example,
-				x = the sensor signal;
-				a = low end of the constraint range;
-				b = high end of the constraint range;
-			@return the constrained signal value:
-				x: if x is between a and b;
-				a: if x is less than a;
-				b: if x is greater than b.
-		*/
-		int read() override;
+    /**
+      Destructor
+      Deletes the origin Sensor instance.
+    */
+    ~ConstrainSensor();
+
+    /**
+      Reads a signal from the origin sensor,
+      constrains the signal and return it.
+      Example,
+      x = the sensor signal;
+      a = low end of the constraint range;
+      b = high end of the constraint range.
+
+      @return the constrained signal value:
+      x: if x is between a and b;
+      a: if x is less than a;
+      b: if x is greater than b.
+    */
+    int read() override;
 };
 
 #endif
